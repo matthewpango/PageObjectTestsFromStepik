21952775fa87d9438a8d34aafe886c441d4860b4
unknown <lionsgatemix@gmail.com>
2021-10-05 07:19:33 +0300

    adding new tests,locators and login page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,3 +1,5 @@
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 link = "http://selenium1py.pythonanywhere.com/"
 
 
@@ -5,6 +7,34 @@ def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open
+    page.go_to_login_page
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_user_should_be_on_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_login_url()
+
+def test_user_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_login_form()
+
+def test_user_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_register_form()
+
+
